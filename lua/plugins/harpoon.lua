f5e349e4a2972e558bf26d7bb406f9526738c3f9
Aron Høyer <aron@hoyer.com>
2024-07-15 09:03:31 +0200

    add harpoon

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -4,10 +4,7 @@ return {
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
     local harpoon = require 'harpoon'
-
-    -- REQUIRED
     harpoon:setup()
-    -- REQUIRED
 
     vim.keymap.set('n', '<leader>a', function()
       harpoon:list():add()
